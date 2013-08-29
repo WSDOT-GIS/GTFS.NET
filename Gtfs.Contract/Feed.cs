2168d7f4f75438ad6acf7c0a6d22b9099a2feee0
JeffJacobson <jeffrey.m.jacobson@github.fake>
2013-08-29 13:17:16 -0700

    Added feed class.

---
+++ Gtfs.Contract/Feed.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gtfs.Contract
+{
+	public class Feed
+	{
+		[Required]
+		public List<Agency> Agency { get; set; }
+
+		[Required]
+		public List<Stop> Stops { get; set; }
+
+		[Required]
+		public List<Route> Routes { get; set; }
+
+		[Required]
+		public List<Trip> Trips { get; set; }
+
+		[Required]
+		public List<StopTime> StopTimes { get; set; }
+
+		[Required]
+		public List<Calendar> Calendar { get; set; }
+
+		public List<CalendarDate> CalendarDates { get; set; }
+
+		public List<FareAttribute> FareAttributes { get; set; }
+
+		public List<FareRule> FareRules { get; set; }
+
+		public List<Shape> Shapes { get; set; }
+
+		public List<Frequency> Frequencies { get; set; }
+
+		public List<Transfer> Transfers { get; set; }
+
+		public FeedInfo FeedInfo { get; set; }
+	}
+}
