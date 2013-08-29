0ec92e968fa05eb8a3d41814c8031fe8c7eee58b
JeffJacobson <jeffrey.m.jacobson@github.fake>
2013-08-29 11:35:17 -0700

    Added Route class and RouteType enum.

---
+++ Gtfs.Contract/Route.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gtfs.Contract
+{
+	public class Route
+	{
+		[Required]
+		public string route_id { get; set; }
+
+		public string agency_id { get; set; }
+
+		[Required]
+		public string route_short_name { get; set; }
+
+		[Required]
+		public string route_long_name { get; set; }
+
+		public string route_desc { get; set; }
+
+		[Required]
+		public RouteType route_type { get; set; }
+
+		public Uri route_url { get; set; }
+
+		public int? route_color { get; set; }
+
+		public int? route_text_color { get; set; }
+	}
+}
---
+++ Gtfs.Contract/RouteType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gtfs.Contract
+{
+	public enum RouteType
+	{
+		TramStreetcarOrLightRail = 0,
+		SubwayOrMetro = 1,
+		Rail = 2,
+		Bus = 3,
+		Ferry = 4,
+		CableCar = 5,
+		Gondola = 6,
+		Funicular = 7
+	}
+}
