35540061fb78140507809d72d38ebff83e72dcf3
JeffJacobson <jeffrey.m.jacobson@github.fake>
2013-08-29 12:29:42 -0700

    Added Calendar class.

---
+++ Gtfs.Contract/Calendar.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gtfs.Contract
+{
+	public class Calendar
+	{
+		[Required]
+		public string service_id { get; set; }
+
+		[Required]
+		public bool monday { get; set; }
+		[Required]
+		public bool tuesday { get; set; }
+		[Required]
+		public bool wednesday { get; set; }
+		[Required]
+		public bool thursday { get; set; }
+		[Required]
+		public bool friday { get; set; }
+		[Required]
+		public bool saturday { get; set; }
+		[Required]
+		public bool sunday { get; set; }
+
+		[Required]
+		public DateTime start_date { get; set; }
+		[Required]
+		public DateTime end_date { get; set; }
+	}
+}
