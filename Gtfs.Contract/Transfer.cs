fc99666543cd2ce891388f4f55df881d5ff35565
JeffJacobson <jeffrey.m.jacobson@github.fake>
2013-08-29 13:06:37 -0700

    Added Transfer class and TransferType enum.

---
+++ Gtfs.Contract/Transfer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gtfs.Contract
+{
+	public class Transfer
+	{
+		[Required]
+		public string from_stop_id { get; set; }
+		[Required]
+		public string to_stop_id { get; set; }
+		[Required]
+		public TransferType? transfer_type { get; set; }
+
+		public uint? min_transfer_time { get; set; }
+	}
+}
---
+++ Gtfs.Contract/TransferType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gtfs.Contract
+{
+	/// <summary>
+	/// The transfer_type field specifies the type of connection for the specified (<see cref="Transfer.from_stop_id"/>, 
+	/// <see cref="Transfer.to_stop_id"/>) pair.
+	/// </summary>
+	public enum TransferType
+	{
+		/// <summary>
+		/// This is a recommended transfer point between two routes.
+		/// </summary>
+		RecommendedTransferPoint = 0,
+		/// <summary>
+		/// This is a timed transfer point between two routes. The departing vehicle is expected to wait for the arriving one, with sufficient time for a passenger to transfer between routes.
+		/// </summary>
+		TimedTransferPoint = 1,
+		/// <summary>
+		/// This transfer requires a minimum amount of time between arrival and departure to ensure a connection. The time required to transfer is specified by <see cref="Transfer.min_transfer_time"/>.
+		/// </summary>
+		MinimumRequired = 2,
+		/// <summary>
+		/// Transfers are not possible between routes at this location.
+		/// </summary>
+		BetweenRouteTransfersImpossible = 3
+	}
+}
