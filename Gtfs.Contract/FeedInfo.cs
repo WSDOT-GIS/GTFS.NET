873bced6903a5e89cbed5beb596d98574b871664
JeffJacobson <jeffrey.m.jacobson@github.fake>
2013-08-29 13:09:34 -0700

    Added FeedInfo class.

---
+++ Gtfs.Contract/FeedInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gtfs.Contract
+{
+	public class FeedInfo
+	{
+		[Required]
+		public string feed_publisher_name { get; set; }
+
+		[Required]
+		public Uri feed_publisher_url { get; set; }
+
+		[Required]
+		public string feed_lang { get; set; }
+
+		public DateTime? feed_start_date { get; set; }
+
+		public DateTime? feed_end_date { get; set; }
+
+		public string feed_version { get; set; }
+	}
+}
