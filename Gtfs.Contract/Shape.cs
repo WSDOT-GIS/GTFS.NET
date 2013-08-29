175baabc0adcd855add2f4c51ac043d45d3f3e12
JeffJacobson <jeffrey.m.jacobson@github.fake>
2013-08-29 12:56:50 -0700

    Added Shape class.

---
+++ Gtfs.Contract/Shape.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gtfs.Contract
+{
+	public class Shape
+	{
+		[Required]
+		public string shape_id { get; set; }
+
+		[Required]
+		public double shape_pt_lat { get; set; }
+
+		[Required]
+		public double shape_pt_lon { get; set; }
+
+		[Required]
+		public uint shape_pt_sequence { get; set; }
+
+		public float shape_dist_traveled { get; set; }
+
+	}
+}
