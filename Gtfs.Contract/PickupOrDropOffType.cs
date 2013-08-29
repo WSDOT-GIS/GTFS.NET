731d27f08ab2f354a0062e2842a5d01042e88c5a
JeffJacobson <jeffrey.m.jacobson@github.fake>
2013-08-29 12:25:34 -0700

    Added StopTime class.

---
+++ Gtfs.Contract/PickupOrDropOffType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gtfs.Contract
+{
+	public enum PickupOrDropOffType
+	{
+		RegularyScheduled = 0,
+		NoneAvailable = 1,
+		MustPhoneAgencyToArrange = 2,
+		MustCoordinateWithDriverToArrange = 3
+	}
+}
---
+++ Gtfs.Contract/StopTime.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gtfs.Contract
+{
+	public class StopTime
+	{
+		[Required]
+		public string trip_id { get; set; }
+
+		[Required]
+		public TimeSpan arrival_time { get; set; }
+
+		[Required]
+		public TimeSpan departure_time { get; set; }
+
+		[Required]
+		public string stop_id { get; set; }
+
+		[Required]
+		public uint stop_sequence { get; set; }
+
+		public string stop_headsign { get; set; }
+
+		public PickupOrDropOffType? pickup_type { get; set; }
+
+		public PickupOrDropOffType? drop_off_type { get; set; }
+
+		public float? shape_dist_travelled { get; set; }
+		
+	}
+}
