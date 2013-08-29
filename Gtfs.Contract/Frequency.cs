e04788d2536fa49c82b4fced6bc9592caa18fe46
JeffJacobson <jeffrey.m.jacobson@github.fake>
2013-08-29 12:59:04 -0700

    Added Frequency class.

---
+++ Gtfs.Contract/Frequency.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gtfs.Contract
+{
+	public class Frequency
+	{
+		[Required]
+		public string trip_id { get; set; }
+
+		[Required]
+		public TimeSpan start_time { get; set; }
+
+		[Required]
+		public TimeSpan end_time { get; set; }
+
+		[Required]
+		public int headway_secs { get; set; }
+
+		public bool? exact_times { get; set; }
+	}
+}
