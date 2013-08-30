eaa08649c1c9977c6ef14a79f937d2b3e1b1ea51
JeffJacobson <jeffrey.m.jacobson@github.fake>
2013-08-30 11:26:40 -0700

    Remove and sort usings.

--- Gtfs.IO/ClassMap/CalendarDateMap.cs
+++ Gtfs.IO/ClassMap/CalendarDateMap.cs
@@ -1,10 +1,5 @@
 ﻿using CsvHelper.Configuration;
 using Gtfs.Contract;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Gtfs.IO.ClassMap
 {
