ebb2bec25767a665dac33d510902ebc44ff03dde
JeffJacobson <Jeffrey.M.Jacobson@gmail.com>
2014-03-05 10:50:36 -0800

    Added BikesAllowed.cs

--- Gtfs.Contract/BikesAllowed.cs
+++ Gtfs.Contract/BikesAllowed.cs
@@ -5,10 +5,20 @@ using System.Text;
 
 namespace Wsdot.Gtfs.Contract
 {
+	/// <summary>
+	/// Indicates if bikes are allowed on a <see cref="Trip"/>.
+	/// </summary>
 	public enum BikesAllowed
 	{
+		/// <summary>No information is available.</summary>
 		NoInformation = 0,
+		/// <summary>
+		/// At least one bike is allowed on the <see cref="Trip"/>.
+		/// </summary>
 		AtLeastOneAllowed = 1,
+		/// <summary>
+		/// No bikes are allowed on the <see cref="Trip"/>.
+		/// </summary>
 		NoBikesAllowed = 2
 	}
 }
